6f674d5a5e774560d755340a9695a080eb232a95
Smith <a045103.dmn1@maclb015803.fmr.com>
2013-04-10 09:58:38 -0600

    Added logging to plan service implementation

--- plan-impl/src/main/java/planimpl/PlanImpl.java
+++ plan-impl/src/main/java/planimpl/PlanImpl.java
@@ -8,21 +8,28 @@
  */
 package planimpl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import plan.Plan;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static java.lang.String.format;
+
 public class PlanImpl implements Plan {
+    static Logger logger = LoggerFactory.getLogger(PlanImpl.class);
     static Map<String,String> planDB = new HashMap<String,String>();
 
     @Override
     public void updatePlan(String user, String plan) {
+        logger.info(format("updatePlan called with %s -> %s", user, plan));
         planDB.put(user,plan);
     }
 
     @Override
     public String getPlan(String user) {
+        logger.info(format("getPlan called for %s", user));
         String plan = planDB.get(user);
         if(plan == null) return ""; else return plan;
     }
