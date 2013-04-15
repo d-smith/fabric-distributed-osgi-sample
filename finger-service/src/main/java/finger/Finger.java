1b3bc881a246ebe5cd11617b2c43d415e3619ce6
d-smith <doug.smith.mail@gmail.com>
2013-04-15 07:57:44 -0600

    Minor clean up

--- finger-service/src/main/java/finger/Finger.java
+++ finger-service/src/main/java/finger/Finger.java
@@ -1,11 +1,4 @@
-/*
- *    Copyright (c) 2011 XTRAC LLC. All Rights Reserved.
- *
- *    This software and all information contained herein is the property of
- *    XTRAC LLC.  Any dissemination, disclosure, use, or reproduction of this
- *    material for any reason inconsistent with express purpose for which it
- *    has been disclosed is strictly forbidden.
- */
+
 package finger;
 
 import plan.Plan;
--- plan-iface/src/main/java/plan/Plan.java
+++ plan-iface/src/main/java/plan/Plan.java
@@ -1,11 +1,3 @@
-/*
- *    Copyright (c) 2011 XTRAC LLC. All Rights Reserved.
- *
- *    This software and all information contained herein is the property of
- *    XTRAC LLC.  Any dissemination, disclosure, use, or reproduction of this
- *    material for any reason inconsistent with express purpose for which it
- *    has been disclosed is strictly forbidden.
- */
 package plan;
 
 public interface Plan {
